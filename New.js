0062a18a0edd60f59a18e2105f039102750f31c7
Wesley Scholl <clients@konjoweb.com>
2019-03-25 17:23:50 -0400

    new form

--- New.js
+++ New.js
@@ -1,11 +1,12 @@
 import React from "react";
 import {
   Button,
-  View,
-  Text,
-  Image,
-  ScrollView,
   StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Keyboard,
   TouchableOpacity
 } from "react-native";
 
@@ -13,18 +14,123 @@ class NewScreen extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      songs: ""
+      title: "",
+      author: "",
+      notes: "",
+      lyrics: ""
     };
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
+
+  handleInputChange(event) {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  handleSubmit(event) {
+    console.log(this.state);
+    console.log(event);
   }
 
   render() {
     return (
-      <ScrollView>
-        <Text style={{ fontSize: 50, justifyContent: "center", padding: 20 }}>
-          New Song Screen
-        </Text>
-      </ScrollView>
+      <View style={styles.container}>
+        <View>
+          <Text style={styles.header}>New Song</Text>
+          <ScrollView>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Title"
+                name="title"
+                onBlur={Keyboard.dismiss}
+                onChange={this.handleInputChange}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Author"
+                name="author"
+                onBlur={Keyboard.dismiss}
+                onChange={this.handleInputChange}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Notes"
+                name="notes"
+                onBlur={Keyboard.dismiss}
+                onChange={this.handleInputChange}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TextInput
+                style={styles.textInput}
+                placeholder="Lyrics"
+                name="lyrics"
+                onBlur={Keyboard.dismiss}
+                onChange={this.handleInputChange}
+              />
+            </View>
+            <View style={styles.inputContainer}>
+              <TouchableOpacity
+                style={styles.saveButton}
+                onPress={this.handleSubmit}
+              >
+                <Text style={styles.saveButtonText}>Save</Text>
+              </TouchableOpacity>
+            </View>
+          </ScrollView>
+        </View>
+      </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 45,
+    backgroundColor: "#F5FCFF"
+  },
+  header: {
+    fontSize: 25,
+    textAlign: "center",
+    margin: 10,
+    fontWeight: "bold"
+  },
+  inputContainer: {
+    paddingTop: 15
+  },
+  textInput: {
+    borderColor: "#CCCCCC",
+    borderTopWidth: 1,
+    borderBottomWidth: 1,
+    height: 50,
+    fontSize: 25,
+    paddingLeft: 20,
+    paddingRight: 20
+  },
+  saveButton: {
+    borderWidth: 1,
+    borderColor: "#007BFF",
+    backgroundColor: "#007BFF",
+    padding: 15,
+    margin: 5
+  },
+  saveButtonText: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    textAlign: "center"
+  }
+});
+
 export default NewScreen;
